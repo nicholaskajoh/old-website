564c454e342ce014595f3382e160b0b83ec39e72
Nicholas Kajoh <kajohterna@gmail.com>
2018-03-20 15:45:02 +0100

    update home page description

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,8 +14,7 @@ const Home = () => (
     <h4>Hello friend...</h4>
 
     <p>
-      I'm Nicholas Kajoh, a Backend Developer Intern at{" "}
-      <a href="https://trivoda.com">Trivoda</a>, Open Sourcerer, Technical
+      I'm Nicholas Kajoh, a Software Developer, Open Sourcerer, Technical
       writer, Community builder at{" "}
       <a href="https://medium.com/covenant-university-developers-community">
         Covenant University Developers' Community
